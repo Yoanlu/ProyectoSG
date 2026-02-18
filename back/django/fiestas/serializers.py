43f1f3d4c2294c9a0ad61f9a997dec0636c997a1
juanluis.rodriguez <juanluis.rodriguez@sandavteam.com>
2026-02-18 12:36:24 +0100

    Traducciones

--- back/django/fiestas/serializers.py
+++ back/django/fiestas/serializers.py
@@ -10,11 +10,8 @@ class FiestaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Fiesta
         fields = ['id', 'festividad', 'inicio', 'fin', 'actividades', 'resumen_actividades']
-
-    # Esta función busca los nombres de las actividades y los junta
+        
     def get_resumen_actividades(self, obj):
-        # 'obj' es la instancia de la Fiesta actual
-        # Sacamos los nombres de todas sus actividades
         nombres = obj.actividades.values_list('nombre', flat=True)
         # Los unimos con una coma
         return ", ".join(nombres) if nombres else "Sin actividades"
